6dc915b44e4ebf939994012e48606f73d957530c
Nikhil Senthilvel <n.sen2000@outlook.com>
2026-06-23 09:07:28 +0000

    Changes

--- 04-analytics-engineering/taxi_rides_ny/models/marts/dim_zones.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/marts/dim_zones.sql
@@ -1,10 +1,7 @@
--- Dimension table for NYC taxi zones
--- This is a simple pass-through from the seed file, but having it as a model
--- allows for future enhancements (e.g., adding calculated fields, filtering)
-
 select
     locationid as location_id,
     borough,
     zone,
-    service_zone
+    replace(service_zone, 'Boro', 'Green') as service_zone
 from {{ ref('taxi_zone_lookup') }}
+
